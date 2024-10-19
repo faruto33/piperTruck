1136c7c9d1b5c5aaa97ef039ee66fb11f76153be
Farid Machrou <fmachrou@gmail.com>
2024-10-20 00:02:06 +0200

    add apache config for production

---
+++ migrations/Version20241019132248.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241019132248 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE foodtruck (id VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE placement (id INT AUTO_INCREMENT NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, foodtruck_id VARCHAR(50) NOT NULL, placement_id INT NOT NULL, date DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\', INDEX IDX_42C84955FD42418B (foodtruck_id), INDEX IDX_42C849552F966E9D (placement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955FD42418B FOREIGN KEY (foodtruck_id) REFERENCES foodtruck (id)');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C849552F966E9D FOREIGN KEY (placement_id) REFERENCES placement (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955FD42418B');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C849552F966E9D');
+        $this->addSql('DROP TABLE foodtruck');
+        $this->addSql('DROP TABLE placement');
+        $this->addSql('DROP TABLE reservation');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
