73c4406267fdd735c41b4701fccb066316fc4789
Farid Machrou <fmachrou@gmail.com>
2024-10-23 08:51:05 +0200

    Forward schedule

--- src/Controller/ScheduleController.php
+++ src/Controller/ScheduleController.php
@@ -10,24 +10,19 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ScheduleController extends AbstractController
 {
-
     #[Route('/schedule', name: 'schedule')]
-    public function index(HttpClientInterface $httpClient,
-                          EntityManagerInterface $entityManager,
-                          Quota $quota): Response
+    public function index(EntityManagerInterface $entityManager, Quota $quota): Response
     {
         // get all placements list
         $placements = $entityManager->getRepository(Placement::class)->findAll();
         // get all foodtrucks list
         $foodtrucks = $entityManager->getRepository(Foodtruck::class)->findAll();
 
-        // call API url to get data
-        $response = $httpClient->request('GET', $this->getParameter('encoded_api_url'));
-
+        // get API json results from reservation controller
+        $response = $this->forward('App\Controller\ReservationController::getAllReservations');
         // $contentType = 'application/json'
         $contents = json_decode($response->getContent(),true);
 
